7fc1a04108d74be24aa0e7c5544eef2db42e4d8b
Brenda Hungerford <bhungerford@codingdojo.com>
2024-03-14 17:36:19 -0400

    rf model

--- pages/Modeling.py
+++ pages/Modeling.py
@@ -65,7 +65,7 @@ st.title('Model Evaluation and Predictions')
 st.subheader('Evaluate the model or make a prediction to the left.')
 
 # Select model with selectbox
-model_name = st.sidebar.selectbox('Select Model', ['random_forest', 'logistic_regression'], index=0)
+model_name = st.sidebar.selectbox('Select Model', ['logistic_regression','random_forest'], index=0)
 model = load_model_ml(FPATHS['models'][model_name])
 
 # Evaluate model subheader and button
