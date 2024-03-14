1ccff0858abb510fae01b91d5ea50ec8f343b9c8
Brenda Hungerford <bhungerford@codingdojo.com>
2024-03-14 17:59:27 -0400

    lowercase

--- pages/.ipynb_checkpoints/Modeling-checkpoint.py
+++ pages/.ipynb_checkpoints/Modeling-checkpoint.py
@@ -64,10 +64,9 @@ st.title('Model Evaluation and Predictions')
 st.subheader('Evaluate the model or make a prediction to the left.')
 
 # Select model with selectbox
-# model_name = st.sidebar.selectbox('Select Model', ['logistic_regression','random_forest'], index=0)
-# Try hard coding in the random_forest
-model = load_model_ml(FPATHS['models']['random_forest'])
-#model = load_model_ml(FPATHS['models'][model_name])
+model_name = st.sidebar.selectbox('Select Model', ['logistic_regression','random_forest'], index=0)
+
+model = load_model_ml(FPATHS['models'][model_name])
 
 # Evaluate model subheader and button
 st.sidebar.subheader('Evaluation')
@@ -77,7 +76,7 @@ labels = ['Approved', 'Rejected']
 
 # When button is pressed
 if st.sidebar.button('Evaluate Model'):
-   #st.subheader(f'Evaluation of {model_name}')
+    st.subheader(f'Evaluation of {model_name}')
     ## Evaluate the model
     train_report, test_report, eval_fig = fn.eval_classification(model, X_train, y_train, X_test, y_test,
                                                              labels=labels)
